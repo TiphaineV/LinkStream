0b41cc24ae75cc819752e46a2e1a91c02cf82a9d
Tiphaine Viard <tiphaine.viard@gmail.com>
2015-01-22 22:47:43 +0100

    Added script for computing dists, ccdfs and plots

---
+++ utils/plots.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+total=`ls stats/values|wc -l|awk '{print($1)}'`
+i=1
+
+for statfile in `ls stats/values`
+do
+    # Compute dist
+    cat 'stats/values/'$statfile |sort -g |uniq -c |awk '{print($2,$1)}' > stats/dists/$statfile-dist
+
+    # Compute ccdf
+    cat stats/dists/$statfile-dist |sort -k1gr,1gr |awk '{t+=$2;print($1,t);}' > stats/dists/$statfile-ccdf
+    # Make plots (gnuplot ?)
+echo "
+set terminal postscript;
+
+# Dists
+set output 'stats/plots/$statfile-dist.eps';
+set multiplot layout 2,2;
+
+plot 'stats/dists/$statfile-dist' notitle;
+
+set logscale x;
+plot 'stats/dists/$statfile-dist' notitle;
+
+unset logscale x;
+set logscale y;
+plot 'stats/dists/$statfile-dist' notitle;
+
+set logscale x;
+plot 'stats/dists/$statfile-dist' notitle;
+
+unset multiplot;
+unset logscale x;
+unset logscale y;
+
+# CCDF
+set output 'stats/plots/$statfile-ccdf.eps';
+set multiplot layout 2,2;
+
+plot 'stats/dists/$statfile-ccdf' notitle;
+
+set logscale x;
+plot 'stats/dists/$statfile-ccdf' notitle;
+
+unset logscale x;
+set logscale y;
+plot 'stats/dists/$statfile-ccdf' notitle;
+
+set logscale x;
+plot 'stats/dists/$statfile-ccdf' notitle;
+
+unset multiplot;
+" > out.gp
+
+    gnuplot out.gp
+    rm out.gp
+
+    echo "Done with file $statfile. ($i/$total)" 1>&2
+    ((i++))
+done
